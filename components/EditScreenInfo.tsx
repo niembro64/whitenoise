7f20e537b9a8030ba62a4408598928402935ce55
Eric Niemeyer <eric.niemeyer@venturetec.net>
2024-10-03 01:30:34 -0400

    back

--- components/EditScreenInfo.tsx
+++ components/EditScreenInfo.tsx
@@ -1,52 +1,77 @@
-import React, { useState } from 'react';
-import { StyleSheet, Button, View } from 'react-native';
-import { Audio } from 'expo-av';
+import React from 'react';
+import { StyleSheet } from 'react-native';
 
-export default function SineWavePlayer() {
-  const [sound, setSound] = useState<Audio.Sound | null>(null);
-  const [isPlaying, setIsPlaying] = useState(false);
+import { ExternalLink } from './ExternalLink';
+import { MonoText } from './StyledText';
+import { Text, View } from './Themed';
 
-  async function playSound() {
-    console.log('Loading Sound');
-    const { sound } = await Audio.Sound.createAsync(
-      require('../assets/sounds/sine440.wav'),
-      { shouldPlay: true, isLooping: true }
-    );
-    setSound(sound);
-    setIsPlaying(true);
+import Colors from '@/constants/Colors';
 
-    console.log('Playing Sound');
-    await sound.playAsync();
-  }
-
-  async function stopSound() {
-    console.log('Stopping Sound');
-    if (sound) {
-      await sound.stopAsync();
-      await sound.unloadAsync();
-      setSound(null);
-      setIsPlaying(false);
-    }
-  }
-
-  const handlePlayStop = () => {
-    if (isPlaying) {
-      stopSound();
-    } else {
-      playSound();
-    }
-  };
-
+export default function EditScreenInfo({ path }: { path: string }) {
   return (
-    <View style={styles.container}>
-      <Button title={isPlaying ? 'Stop' : 'Play'} onPress={handlePlayStop} />
+    <View>
+      <View style={styles.getStartedContainer}>
+        <Text
+          style={styles.getStartedText}
+          lightColor="rgba(0,0,0,0.8)"
+          darkColor="rgba(255,255,255,0.8)">
+          Open up the code for this screen:
+        </Text>
+
+        <View
+          style={[styles.codeHighlightContainer, styles.homeScreenFilename]}
+          darkColor="rgba(255,255,255,0.05)"
+          lightColor="rgba(0,0,0,0.05)">
+          <MonoText>{path}</MonoText>
+        </View>
+
+        <Text
+          style={styles.getStartedText}
+          lightColor="rgba(0,0,0,0.8)"
+          darkColor="rgba(255,255,255,0.8)">
+          Change any of the text, save the file, and your app will automatically update.
+        </Text>
+      </View>
+
+      <View style={styles.helpContainer}>
+        <ExternalLink
+          style={styles.helpLink}
+          href="https://docs.expo.io/get-started/create-a-new-app/#opening-the-app-on-your-phonetablet">
+          <Text style={styles.helpLinkText} lightColor={Colors.light.tint}>
+            Tap here if your app doesn't automatically update after making changes
+          </Text>
+        </ExternalLink>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    marginTop: 50,
+  getStartedContainer: {
     alignItems: 'center',
+    marginHorizontal: 50,
+  },
+  homeScreenFilename: {
+    marginVertical: 7,
+  },
+  codeHighlightContainer: {
+    borderRadius: 3,
+    paddingHorizontal: 4,
+  },
+  getStartedText: {
+    fontSize: 17,
+    lineHeight: 24,
+    textAlign: 'center',
+  },
+  helpContainer: {
+    marginTop: 15,
+    marginHorizontal: 20,
+    alignItems: 'center',
+  },
+  helpLink: {
+    paddingVertical: 15,
+  },
+  helpLinkText: {
+    textAlign: 'center',
   },
 });
